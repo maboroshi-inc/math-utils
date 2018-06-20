9a39fca8ff704b496081d6cffd1d08f91f7f82d3
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-20 17:43:01 +0900

    update documents.

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,21 +1,3 @@
-var typedoc = typedoc || {}
-typedoc.search = typedoc.search || {}
-typedoc.search.data = {
-  kinds: { '64': 'Function' },
-  rows: [
-    {
-      id: 0,
-      kind: 64,
-      name: 'sum',
-      url: 'globals.html#sum',
-      classes: 'tsd-kind-function'
-    },
-    {
-      id: 1,
-      kind: 64,
-      name: 'average',
-      url: 'globals.html#average',
-      classes: 'tsd-kind-function'
-    }
-  ]
-}
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"sum","url":"globals.html#sum","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"average","url":"globals.html#average","classes":"tsd-kind-function"}]};
