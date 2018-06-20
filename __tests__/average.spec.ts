41392dd8f42a1af07fd2deaceb647ece5e12ab5e
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-20 17:59:47 +0900

    `average()` のドキュメントとテストを日本語化した。

--- __tests__/average.spec.ts
+++ __tests__/average.spec.ts
@@ -1,7 +1,7 @@
 import { average } from '../src'
 
-describe('average', () => {
-  it('returns the average of values', () => {
+describe('average()', () => {
+  it('全ての引数の平均値を返す', () => {
     expect(average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).toEqual(5.5)
   })
 })
--- src/average.ts
+++ src/average.ts
@@ -1,10 +1,10 @@
 import { sum } from './sum'
 
 /**
- * Get the average of values.
+ * 平均値を返す
  *
- * @param values Numbers to calculate average.
- * @returns The average of `values`
+ * @param values （可変長）平均値を求めたい数値の集合
+ * @returns `values` の平均値
  */
 export function average(...values: number[]) {
   return sum(...values) / values.length
