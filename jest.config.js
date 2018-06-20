d4ff0f9951c52a6ae01cc52394b95d40f02e5ba5
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-20 13:01:00 +0900

    set environments for unit testing.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.json'
+    }
+  }
+}
