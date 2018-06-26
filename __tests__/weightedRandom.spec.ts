2555fce44bd65730dad283a052c7001f6f4e5324
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-26 21:49:20 +0900

    [WIP] `weightedRandom()` を実装した。

---
+++ __tests__/weightedRandom.spec.ts
@@ -0,0 +1,26 @@
+import { map, weightedRandom } from '../src'
+
+describe('weightedRandom()', () => {
+  it('10万個の乱数の発生割合が重み付けリストの内容に収束する', () => {
+    const weights = [5, 2, 3, 1, 4]
+    const counts = [0, 0, 0, 0, 0]
+
+    let r
+
+    for (let i = 0; i < 100000; i++) {
+      r = weightedRandom(weights)
+      counts[r] += 1
+    }
+
+    const min1 = Math.min(...counts)
+    const max1 = Math.max(...counts)
+    const min2 = Math.min(...weights)
+    const max2 = Math.max(...weights)
+
+    return Promise.all(
+      counts.map((val, i) =>
+        expect(map(val, min1, max1, min2, max2)).toBeCloseTo(weights[i], 0)
+      )
+    )
+  })
+})
---
+++ src/weightedRandom.ts
@@ -0,0 +1,31 @@
+import { sum } from './'
+
+/**
+ * 重み付け乱数を選択する
+ * `Math.random()` とは異なり **整数値** を返す
+ *
+ * @param weights 重み付けリスト
+ * @returns `weights` に基づいてランダムに `weights` のインデックスを返す
+ * @todo `weights` が長大である場合に2分探索させる
+ */
+export function weightedRandom(weights: number[]) {
+  const sortedWeights = Array.from(weights).sort()
+  const totalWeight = sum(...sortedWeights)
+  const r = Math.random() * totalWeight
+
+  let memo = 0
+  let index = -1
+
+  sortedWeights.some((w, i) => {
+    memo += w
+
+    if (r < memo) {
+      index = i
+      return true
+    }
+
+    return false
+  })
+
+  return weights.indexOf(sortedWeights[index])
+}
