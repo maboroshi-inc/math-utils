499241dac1aa3c2348c86700769aa40297e8280a
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-06-29 11:59:16 +0900

    rollup-plugin-filesize をインストールした。

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs'
+import filesize from 'rollup-plugin-filesize'
 import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 
@@ -31,6 +32,7 @@ export default {
       main: true,
       browser: true
     }),
-    commonjs()
+    commonjs(),
+    filesize()
   ]
 }
