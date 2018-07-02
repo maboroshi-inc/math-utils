acae4705fc9a25c3733dd18ecc372fb6ec6e280d
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-07-02 18:32:18 +0900

    lodash のために必要だった環境設定を取り除いた。

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
 import progress from 'rollup-plugin-progress'
-import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
@@ -22,17 +21,11 @@ export default {
     typescript({
       tsconfigOverride: {
         compilerOptions: {
-          baseUrl: 'src',
           module: 'es2015',
           moduleResolution: 'node'
         },
         exclude: ['__tests__']
       }
-    }),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true
     }),
     commonjs(),
     filesize()
