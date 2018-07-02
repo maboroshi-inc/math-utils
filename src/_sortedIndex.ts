10bc4796b3161a1074e629d801600a1092988589
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-07-02 12:16:43 +0900

    非公開関数 `_sortedIndex()` を実装した。

---
+++ src/_sortedIndex.ts
@@ -0,0 +1,21 @@
+/**
+ * @hidden 指定値を挿入しうる配列中の位置を2分探索で特定する
+ *
+ * @param array 調査対象配列
+ * @param value 評価値
+ */
+export function _sortedIndex(array: number[], value: number): number {
+  const { length } = array
+
+  if (length > 1) {
+    const mid = length >> 1 // @see https://github.com/darkskyapp/binary-search
+
+    if (value < array[mid]) {
+      return _sortedIndex(array.slice(0, mid), value)
+    } else {
+      return mid + _sortedIndex(array.slice(mid), value)
+    }
+  }
+
+  return length === 1 && array[0] < value ? 1 : 0
+}
